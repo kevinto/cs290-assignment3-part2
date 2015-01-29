ba6f0c531914a11c3e784dec820b30a1e1f9dae0
U-OMNICELL\kevint <kevint@OMCL764-0HHLFAH.omnicell.com>
2015-01-28 20:29:27 -0800

    Created sample code to call the gist API.

--- demo.js
+++ demo.js
@@ -13,3 +13,25 @@ function clearLocalStorage() {
 function displayLocalStorage() {
 	document.getElementById('output').innerHTML = localStorage.getItem('demoText');
 }
+
+function getGists() {
+	var request = new XMLHttpRequest();
+	if (!request) {
+		throw 'Unable to create HttpRequest.';
+	}
+
+	var url = 'https://api.github.com/gists?page=1'
+	request.open('GET', url);
+	request.send();
+
+	// Test code to get the data from different pages
+	var request2 = new XMLHttpRequest();
+	var url = 'https://api.github.com/gists?page=2'
+	request2.open('GET', url);
+	request2.send();
+}
+
+// Gets the gists onload first
+	// Store them on the page or in local storage
+	// Can store them as different page objects?
+getGists();
