9c17607a83f5ca39939e8061f05687ac3ace1075
U-OMNICELL\kevint <kevint@OMCL764-0HHLFAH.omnicell.com>
2015-01-29 18:58:58 -0800

    Added the javascript to dynamically create a list item for each gist description

--- demo.js
+++ demo.js
@@ -9,6 +9,8 @@ window.onload = function() {
   //     localStorage.setItem(pageName, JSON.stringify(pageInitValue));
   //   }
   // }
+  // TODO: neeed add the call here for page storage
+turnJSONtoOjb();
 }
 
 function saveDemoInput() {
@@ -25,8 +27,8 @@ function displayLocalStorage() {
 
 function getGists() {
 	var request = new XMLHttpRequest();
-	
-  for (var i = 0; i < 5; i++) {
+  var numberOfPages = 1;	
+  for (var i = 0; i < numberOfPages; i++) {
     requestGistPage(i);
 	}
 	// var url = 'https://api.github.com/gists?page=1'
@@ -54,7 +56,61 @@ function requestGistPage(pageNumber) {
   request.open('GET', url);
   request.send(); 
 }
+
 // Gets the gists onload first
 	// Store them on the page or in local storage
 	// Can store them as different page objects?
-getGists();
+//getGists(); // This means the API requests
+
+function turnJSONtoOjb() {
+    var pageInfo = null;
+    while (pageInfo === null) {
+      pageInfo = localStorage.getItem('page0');
+      break;
+    } 
+
+    var firstStringify = JSON.parse(pageInfo);
+    var finalObjectList = JSON.parse(firstStringify);
+
+    ul = document.getElementById('gist-list')
+    createGistList(document.getElementById('gist-list'), finalObjectList);
+}
+
+function createGistList(ul, objectList) {
+  objectList.forEach(function(s) {
+    var li = document.createElement('li');
+    li.appendChild(liDesc(s));
+    ul.appendChild(li);
+  });
+}
+
+function liDesc(gist) {
+  var dl = document.createElement('dl');
+  var entry = dlEntry('Desc: ', gist);
+  dl.appendChild(entry.dt);
+  dl.appendChild(entry.dd);
+  return dl;
+}
+
+function dlEntry(term, gist) {
+  var dt = document.createElement('dt');
+  var dd = document.createElement('dd');
+  dt.innerText = term;
+
+  var a = document.createElement('a');
+
+  var aText = document.createTextNode(gist.description);
+  if (gist.description === "") {
+    aText = document.createTextNode('This gist has no description!!!');
+  };
+
+  a.setAttribute('href', gist.html_url);
+  a.appendChild(aText);
+  dd.appendChild(a);
+  //dd.innerText = gist.description;
+  //dd.baseURI = gist.html_url;
+  return  {'dt':dt, 'dd':dd};
+}
+//turnJSONtoOjb();
+
+// Lets display the results
